12f48090069db91e7f6722a10f19ef255c68d5b7
alonanour <156347994+alonanour@users.noreply.github.com>
2024-02-05 16:27:46 +0200

    fix homework 3

--- homework_3_1.py
+++ homework_3_1.py
@@ -1,17 +1,27 @@
+# Завдання 1 Найпростіший калькулятор
+#
+# Програма має виконувати прості математичні дії (+, -, *, /).
+# Користувачеві пропонується почерзі ввести числа та дію над цими числами, а програма, виходячи з дії,
+# обчислює та друкує результат.
+#
+# Зробити перевірку на те, що при діленні дільник не дорівнює 0!
+
 number_1 = input("Введіть перше число: ")
 number_2 = input("Введіть друге число: ")
 act = input("Напишіть яку дію над цими числами ви хочете зробити: ")
 
 if act == "додавання" or act == "додати" or act == "+":
-    print(float(number_1) + float(number_2))
+    result = float(number_1) + float(number_2)
 elif act == "віднімання" or act == "відняти" or act == "-":
-    print(float(number_1) - float(number_2))
+    result = float(number_1) - float(number_2)
 elif act == "множення" or act == "помножити" or act == "*":
-    print(float(number_1) * float(number_2))
+    result = float(number_1) * float(number_2)
 elif act == "ділення" or act == "поділити" or act == ":" or act == "/":
     if number_2 == "0":
-        print("Дільник не може дорівнювати нулю")
+        result = "Дільник не може дорівнювати нулю"
     else:
-        print(float(number_1) / float(number_2))
+        result = float(number_1) / float(number_2)
 else:
-    print("Ви зробили щось не так")
+    result = "Ви зробили щось не так"
+
+print(result)
