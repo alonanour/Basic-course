c4bed4a5363331e6f2447cc507f86e54e67b727b
alonanour <156347994+alonanour@users.noreply.github.com>
2024-02-21 18:48:14 +0200

    fix homework 10

--- homework_10_2.py
+++ homework_10_2.py
@@ -15,10 +15,7 @@
 # print('OK')
 
 def is_even(digit):
-    if digit % 2 == 0:
-        return True
-    else:
-        return False
+    return digit % 2 == 0
 
 
 assert is_even(2) == True, 'Test1'
